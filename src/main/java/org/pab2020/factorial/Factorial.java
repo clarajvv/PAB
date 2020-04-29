c8a2b20169c8571ccb3c63af854fa1f8f7292efc
clarajvv <61667090+clarajvv@users.noreply.github.com>
2020-04-29 13:44:11 +0200

    modificar compute:version clase

--- src/main/java/org/pab2020/factorial/Factorial.java
+++ src/main/java/org/pab2020/factorial/Factorial.java
@@ -8,24 +8,22 @@ package org.pab2020.factorial;
  */
 public class Factorial {
 
-    public long compute(long value) {
+    public long compute(long value){
         long result;
-        if (value < 0) {
-            throw new RuntimeException("Negative number: " + value);
-        } else if ((value == 0) || (value == 1)) {
+        if (value<0){
+            throw new RuntimeException();
+        }
+        else if((value ==0)|| (value == 1)){
             result = 1;
-        } else if (value == 2) {
-            result = 2;
-        } else {
-            //result = value * compute(value - 1);
-            result=value;
-            while(value-1>1){
-                result=result*(value-1);
-                value=value-1;
         }
-
+        else{
+            // Computacionalmente mejor usar un for
+            //result = value * compute(value-1);
+            result = 1;
+            for (int i = 1; i<=value; i++){
+                result = i* result;
+            }
         }
-
         return result;
     }
 }
