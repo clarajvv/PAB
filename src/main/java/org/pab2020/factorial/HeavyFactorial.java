4d93fe4c4948b18645326118dca20b4ef6680c72
clarajvv <61667090+clarajvv@users.noreply.github.com>
2020-05-05 13:39:36 +0200

    heavy y parallel factorial

--- src/main/java/org/pab2020/factorial/HeavyFactorial.java
+++ src/main/java/org/pab2020/factorial/HeavyFactorial.java
@@ -1,13 +1,22 @@
 package org.pab2020.factorial;
-
 public class HeavyFactorial extends Factorial {
+
+    private long iterations ;
+
+    public HeavyFactorial() {
+        this(100000) ;
+    }
+
+    public HeavyFactorial(long iterations) {
+        this.iterations = iterations ;
+    }
 
     @Override
     public long compute(long value) {
-        for (long i = 0; i < 5000000; i++) {
-            double dummy = Math.sin(2.334) * Math.cos(i - 0.23);
+        for (long i = 0 ; i < iterations; i++) {
+            double dummy = Math.sin(2.352 +i) * Math.cos(i - 0.235) ;
         }
 
-        return super.compute(value);
+        return super.compute(value) ;
     }
 }
--- src/main/java/org/pab2020/parallel/ParallelFactorial.java
+++ src/main/java/org/pab2020/parallel/ParallelFactorial.java
@@ -17,31 +17,48 @@ import java.util.stream.Collectors;
 public class ParallelFactorial {
     public static void main(String[] args1){
         List<Integer> numbers = List.of(2,3,4,5,6,7,10,20,30,40);
+        List<List<Long>> factorialValues;
         //List<Integer> numbers = Arrays.asList(2,3,4,5,6,7,10,20,30,40);
 
-        List<Long> factorialValues = new ArrayList<>();
+        //List<Long> factorialValues = new ArrayList<>();
 
-        HeavyFactorial factorial = new HeavyFactorial();
+        HeavyFactorial factorial = new HeavyFactorial( 500000);
+//en paralelo tardo 2s 622 ms, y con stream a secas
+        //el tiempo siempre es distinto porque la carga del sistema nunca es la misma
+
+        int numberOfCoresToUse=8;
+        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelisa", "" +numberOfCoresToUse);
+
 
         long initTime = System.currentTimeMillis();
 
         factorialValues=numbers
-                .parallelStream()
-                .map(number -> factorial.compute(number)) //para cada elemento, me transforma el elemento a otro nuevo
-                .collect(Collectors.toList());
+                .stream()//con parallelStream() esto tarda mucho menos que en stream()
+                .filter(number ->(number % 2)==0)//me devuelve números pares
+                .map(number -> {//numero y factorial correspondiente
+                    System.out.println(number);
+                    return List.of(Long.valueOf(number), factorial.compute(number));
+                }) //para cada elemento, me transforma el elemento a otro nuevo
+                .collect(Collectors.toList()); //deja de inicializarse factorialValues
 
         /*for(int i=0; i<numbers.size(); i++){
             factorialValues.add(factorial.compute(numbers.get(i)));
         }*/
 
+        /*
+        for(Integer number:numbers){
+            factorialValues.add(factorial.compute(number));
+        }
+*/
 
-        long totalTime = System.currentTimeMillis();
+        long totalTime = System.currentTimeMillis() - initTime;
 
         System.out.println("Total computing time: " + totalTime);
 
         for (int i=0; i<factorialValues.size(); i++){
-            System.out.println(("Number: "+numbers.get(i) + ". Factorial: "+factorialValues.get(i)));
+            System.out.println(("Number: "+factorialValues.get(i).get(0) + ". Factorial: "+factorialValues.get(i).get(1)));
         }
+
 
 
     }
